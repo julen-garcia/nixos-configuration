3f0b06e5175d336ebb7901e50c53e1c3b5b8a8fa
Julen Garcia <76101410+julen-garcia@users.noreply.github.com>
2025-11-06 20:30:29 +0100

    fix: solve problem with the yubikey notifier

--- modules/hardware/yubikey.nix
+++ modules/hardware/yubikey.nix
@@ -12,14 +12,21 @@
     yubikey-manager
   ];
 
-  # Get a notification when the yubikey is waiting for a touch
-  systemd.user.services.yubikey-touch-detector = {
-    description = "Yubikey touch detector";
-    wantedBy = [ "multi-user.target" ];
-    serviceConfig = {
-      ExecStart = "${pkgs.yubikey-touch-detector}/bin/yubikey-touch-detector --libnotify";
-    };
+  programs.yubikey-touch-detector = {
+    enable = true;
+    # unixSocket = false;
+    # libnotify = true;
   };
+
+  # The notifier works without this
+  # # Get a notification when the yubikey is waiting for a touch
+  # systemd.user.services.yubikey-touch-detector = {
+  #   description = "Yubikey touch detector";
+  #   wantedBy = [ "multi-user.target" ];
+  #   serviceConfig = {
+  #     ExecStart = "${pkgs.yubikey-touch-detector}/bin/yubikey-touch-detector --libnotify";
+  #   };
+  # };
 
   # Allow use of sudo with yubikey
   security.pam.services = {
