c2a4035bb0b815a78d689d196e66567101a19be8
julen_port <76101410+julen-garcia@users.noreply.github.com>
2026-04-11 15:23:02 +0200

    chore: upgrade versions

--- modules/myservices/reverse-proxy.nix
+++ modules/myservices/reverse-proxy.nix
@@ -112,7 +112,7 @@ in
       group = cfg.group;
       package = pkgs.caddy.withPlugins {
         plugins = [ "github.com/caddy-dns/cloudflare@v0.2.4" ];
-        hash = "sha256-pRrLBlYRaAyMYwPXeTy4WqWNRu/L9K6Mn2src11dGh8=";
+        hash = "sha256-4WF7tIx8d6O/Bd0q9GhMch8lS3nlR5N3Zg4ApA3hrKw=";
       };
       globalConfig = 
       ''
