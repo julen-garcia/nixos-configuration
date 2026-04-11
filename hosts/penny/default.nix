a318737bbd81c81f0c9f4df8b5fd1c1b83175e10
julen_port <76101410+julen-garcia@users.noreply.github.com>
2026-04-11 15:54:51 +0200

    chore: add missing token and config for ntfy in penny

--- hosts/penny/default.nix
+++ hosts/penny/default.nix
@@ -19,6 +19,13 @@
     ./services/ntfy.nix
   ];
 
+  # Import the needed secrets
+  sops.secrets = {
+    "ntfy/penny-zfs-token" = {
+      sopsFile = ./secrets.yaml;
+    };
+  };
+
   # Create a swap file for hibernation.
   swapDevices = [
     {
@@ -37,9 +44,13 @@
   boot.loader.efi.canTouchEfiVariables = true;
 
   # ZFS related options
-  boot.supportedFilesystems = [ "zfs" ];
   zfs = {
     enable = true;
+    ntfy = {
+      enable = true;
+      topic = "penny";
+      tokenFile = config.sops.secrets."ntfy/penny-zfs-token".path;
+    };
   };
   boot.zfs.extraPools = [ "zstorage" ];
 
