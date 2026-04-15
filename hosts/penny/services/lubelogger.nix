cb7dfc93345a31d383137df3b31c3ec9ac1b9233
Julen Garcia <76101410+julen-garcia@users.noreply.github.com>
2026-04-15 20:58:10 +0200

    chore: add lubelloger without https proxy

---
+++ hosts/penny/services/lubelogger.nix
@@ -0,0 +1,43 @@
+{ lib, config, pkgs, ... }:
+let
+  version = "v1.6.3";
+  dataPath = "/var/lib/lubelogger";
+  port = 5001;
+  user = "lubelogger";
+  containerService = "podman-lubelogger";
+  backupPath = "/zstorage/internal-backups/lubelogger/";
+in {
+
+  users.groups.${user} = {};
+  users.users.${user} = {
+    group = "${user}";
+    isSystemUser = true;
+  };
+
+  systemd.tmpfiles.rules = [
+    "d ${dataPath} 0750 ${user} ${user}"
+    "d ${backupPath} 0750 ${user} ${user}"
+  ];
+
+  virtualisation.oci-containers.containers.lubelogger = {
+    image = "ghcr.io/hargata/lubelogger:${version}";
+    ports = [
+      "${toString port}:8080"
+    ];
+
+    volumes = [
+      "${dataPath}:/data"
+    ];
+
+    environment = {
+      PUID = toString config.users.users.${user}.uid;
+      PGID = toString config.users.groups.${user}.gid;
+      LC_ALL = "es_ES";
+      LANG = "es_ES";
+    };
+  };
+}
+
+
+
+
