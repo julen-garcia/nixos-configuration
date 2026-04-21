aa5993186502175a8aa1314ea19eb9caa081e346
Julen Garcia <76101410+julen-garcia@users.noreply.github.com>
2026-04-21 19:05:04 +0200

    feat: add samba password from sops

--- hosts/penny/services/samba.nix
+++ hosts/penny/services/samba.nix
@@ -1,5 +1,11 @@
-{ ... }:
-{
+{   config,
+  pkgs,
+  ...
+}: let
+  username = "julen";
+  ifGroupExist = groups: builtins.filter (group: builtins.hasAttr group config.users.groups) groups;
+  smbSecretPath = config.sops.secrets."${username}-smb-password".path;
+in {
   services.samba = {
     enable = true;
     openFirewall = true;
@@ -46,4 +52,26 @@
   };
 
   networking.firewall.allowPing = true;
+
+  # Samba Password
+  # Import the needed secrets
+  sops = {
+    secrets = {
+      "julen-smb-password" = {
+        sopsFile = ../../common/secrets.yaml;
+      };
+    };
+  };
+
+  system.activationScripts = {
+    # The "init_smbpasswd" script name is arbitrary, but a useful label for tracking
+    # failed scripts in the build output. An absolute path to smbpasswd is necessary
+    # as it is not in $PATH in the activation script's environment. The password
+    # is repeated twice with newline characters as smbpasswd requires a password
+    # confirmation even in non-interactive mode where input is piped in through stdin. 
+    "init_${username}_smbpasswd".text = ''
+      /run/current-system/sw/bin/printf "$(/run/current-system/sw/bin/cat ${smbSecretPath})\n$(/run/current-system/sw/bin/cat ${smbSecretPath})\n" | ${pkgs.samba}/bin/smbpasswd -sa ${username}
+    '';
+  };
 }
+
