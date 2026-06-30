b0605f69444dbdd9e8f6d702c90e098f6079e1e9
julen_port <76101410+julen-garcia@users.noreply.github.com>
2026-06-30 12:37:05 +0200

    chore: update hash for caddy

--- modules/myservices/reverse-proxy.nix
+++ modules/myservices/reverse-proxy.nix
@@ -112,7 +112,7 @@ in
       group = cfg.group;
       package = pkgs.caddy.withPlugins {
         plugins = [ "github.com/caddy-dns/cloudflare@v0.2.4" ];
-        hash = "sha256-8yZDrejNKsaUnUaTUFYbarWNmxafqp2z2rWo+XRsxV8=";
+        hash = "sha256-hEHgAG0F0ozHRAPuxEqLyTATBrE+pajeXDiSNwniorg=";
       };
       globalConfig = 
       ''
